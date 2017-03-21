e2e98633cc2c2866d5218b62aaa2e086d5ccd014
Daniel Andrei Minca <mandrei17@gmail.com>
2017-03-21 12:47:41 +0200

    add TCP server implementation in python
    Resolves:
    Related:
    Signed-off-by: Daniel Andrei Minca <mandrei17@gmail.com>

---
+++ tcp_server.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python2
+# SIMPLE TCP SERVER IMPLEMENTATION IN PYTHON
+
+import socket
+import threading
+
+bind_ip = '0.0.0.0'
+bind_port = 9999
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind((bind_ip, bind_port))
+
+server_listen(5)
+
+print "[*] Listening on %s:%d" % (bind_ip, bind_port)
+
+# client-handling thread
+def handle_client(client_socket):
+
+# print what client sends
+    request = client_socket.recv(1024)
+
+    print "[*] Received: %s" % request
+
+# send back packet
+    client_socket.send("ACK!")
+
+    client_socket.close()
+
+while True:
+
+    client,addr = server.accept()
+
+    print "[*] Accepted connection from: %s:%d" % (addr[0], addr[1])
+
+# spin client thread to handle incomming data
+    client_handler = threading.Thread(target = handle_client, args = (client,))
+    client_handler.start()
+
