a488e9d67dab5a5763ed839db8853ac89e8c2cc0
Daniel Andrei Minca <mandrei17@gmail.com>
2017-03-21 17:50:52 +0200

    add primary server loop and stub function
    * the stub function is for handling both command execution and full
      command shell
    
    Resolves:
    Related:
    Signed-off-by: Daniel Andrei Minca <mandrei17@gmail.com>

--- netcat.py
+++ netcat.py
@@ -132,5 +132,37 @@ def client_sender(buffer):
 # tear down connection
         client.close()
 
+def server_loop():
+    global target
+
+# if no target defined, listen on all interfaces
+    if not len(target):
+        target = "0.0.0.0"
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((target, port))
+    server.listen(5)
+
+    while True:
+        client_socket, addr = server.accept()
+
+# spin off thread to handle new client
+        client_thread = threading.Thread(target = client_handler, args = (client_socket,))
+        client_thread.start()
+
+def run_command(command):
+
+# trim newline
+    command = command.rstrip()
+
+# run cmd and get output
+    try:
+        output = subprocess.check_output(command, stderr = subprocess.STDOUT, shell = True)
+    except:
+        output = "Failed to execute command.\r\n"
+
+# send output back to client
+    return output
+
 main()
 
