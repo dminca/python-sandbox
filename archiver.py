cfbd6ede4e8561c09f497ae2d41161a14ab8df09
Daniel Andrei Minca <mandrei17@gmail.com>
2017-05-28 12:14:37 +0300

    backup thunderbird filters
    Resolves:
    Related:
    Signed-off-by: Daniel Andrei Minca <mandrei17@gmail.com>

---
+++ archiver.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# BACKUP THUNDERBIRD FILTERS
+
+from time import gmtime, strftime
+import tarfile
+import os
+
+# get current date
+current_date = strftime("%Y-%m-%d", gmtime())
+
+# find files and store in array
+def find_all(name, path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            result.append(os.path.join(root, name))
+    return result
+
+# create archive
+#tar = tarfile.open(current_date + ".tar.gz", "w:gz")
+#for name in ["file1", "file2", "file3"]:
+#    tar.add(name)
+#tar.close()
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
